41c6f824e767b8734a020e7e79e13bc448402585
AliCodesJava <ali.ottawa.canada@gmail.com>
2022-05-25 00:06:03 -0400

    -à

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # uo-outreach-frontend-interview-challenge
+
+## Your Name
+
+Ali ABDEDDAIM
+
+## Which challenge did you attempt?
+
+Front-end
+
+## Technologies used
+
+- HTML
+- SCSS
+- JavaScript
+
+## Source Code URL
+
+Here
+
+## Hosted URL
+
+To do
+
+## Goals implemented
+
+## Run instructions
+    1. Clone my repo onto your machine and cd into it
+    2. Run 'ng serve -o'
+    3. Give it some time, then the application should be accessible at http://localhost:4200/
+
+## Anything else you would like us to know?
---
+++ js/index.js
@@ -0,0 +1,33 @@
+function request(method, uri, dict){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open(method, uri, false);
+    xmlhttp.send(dict);
+    console.log(xmlhttp.response);
+    // const response = fetch(
+    //   uri,
+    //   {
+    //     mode: 'cors',
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //     },
+    //     body: dict,
+    //   } 
+    // );
+    // return response
+}
+
+function checkCodeInput(){
+    const mfaCodeInput = document.getElementById("mfa-code");
+    const status = document.getElementById("status");
+    if(mfaCodeInput.value.search(/([0-9]{6})/) == -1){
+        status.innerHTML = "Code format invalid!"
+    }
+    return;
+}
+
+window.onload = function(){
+    r=request("POST", "https://cors-anywhere.herokuapp.com/https://coop-interview.outstem.io/validate/", {code:"123456"});
+    console.log(r);
+    checkCodeInput();
+}
